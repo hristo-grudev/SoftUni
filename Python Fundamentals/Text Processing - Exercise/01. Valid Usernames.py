483f45304cad7fe32f733a9dca8ba22e7daae130
Hristo Grudev <75523508+hristo-grudev@users.noreply.github.com>
2021-03-19 09:19:24 +0200

    Update

--- Python Fundamentals/Text Processing - Exercise/01. Valid Usernames.py
+++ Python Fundamentals/Text Processing - Exercise/01. Valid Usernames.py
@@ -1,17 +1,5 @@
-import sqlite3, datetime
-def runSQL(sql):
-	conn = sqlite3.connect(r'C:\Users\hristo.grudev\Projects\recipes\recipes.db')
-	cursor = conn.cursor()
-	cursor.execute(sql)
-	data = cursor.fetchall()
-	cursor.close()
-	conn.close()
-	return data
+usernames = input().split(', ')
 
-birthday_sql = '''select name, date from holidays
-where strftime('%d.%m', date) = strftime('%d.%m', date('now', '6 day'))'''
-birthdays = runSQL(birthday_sql)
-for birthday in birthdays:
-	name, date = [*birthday]
-	today = datetime.datetime.now().strftime('%Y')
-	years = int(today) - int(date[:4])
+for user in usernames:
+	if 3 <= len(user) <= 16 and (user.isalnum() or "_" in user or "-" in user):
+		print(user)
