5ebc069c8110ad271b5343637c44df36dbfefeed
hristo-grudev <hr.grudev@gmail.com>
2021-05-27 16:12:20 +0300

    8

--- Python Advanced/Comprehensions - Exercise/08. Heroes Inventory.py
+++ Python Advanced/Comprehensions - Exercise/08. Heroes Inventory.py
@@ -2,16 +2,14 @@ names = input().split(', ')
 
 data = input()
 
-heroes = {}
+heroes = {name: {} for name in names}
 while not data == 'End':
 	name, item, price = data.split('-')
-	if not heroes.get(name) and name in names:
-		heroes[name] = {}
 	if not heroes[name].get(item):
 		heroes[name].update({item: int(price)})
 
 	data = input()
 
 print(
-	*[f'{name} -> {len(items)}, Cost: {sum(items[item] for item in items)}' for name, items in heroes.items()],
+	*[f'{name} -> Items: {len(items)}, Cost: {sum(items[item] for item in items)}' for name, items in heroes.items()],
 	sep='\n')
