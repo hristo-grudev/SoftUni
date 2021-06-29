1daabd15c9bec514052220d622bed6abb8f4d760
hristo-grudev <hr.grudev@gmail.com>
2021-06-29 13:23:12 +0300

    class

---
+++ Python OOP/First Steps in OOP - Lab/01. Rhombus of Stars.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+
+def draw_line(count, symbol, offset_count=0):
+	offset = offset_count * ' '
+	content = (symbol * count).strip()
+	print(f'{offset}{content}')
+
+
+def draw_rhombus(n):
+	for i in range(n):
+		draw_line(i + 1, '* ', n - i - 1)
+
+	for i in range(n - 2, -1, -1):
+		draw_line(i + 1, '* ', n - i - 1)
+
+
+# def draw_triangle(n):
+# 	for i in range(n):
+# 		draw_line(i + 1, '+')
+# 	for i in range(n - 2, -1, -1):
+# 		draw_line(i + 1, '+')
+
+
+draw_rhombus(n)
---
+++ Python OOP/First Steps in OOP - Lab/03. Class Book.py
@@ -0,0 +1,5 @@
+class Book():
+	def __init__(self, name, author, pages):
+		self.pages = pages
+		self.author = author
+		self.name = name
