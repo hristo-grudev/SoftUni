191bfe52e1e8302f3f1e0fded79505286438a4a1
hristo-grudev <hr.grudev@gmail.com>
2021-07-29 16:13:19 +0300

    animals

--- Python OOP/Polymorphism and Abstraction - Exercise/04. Wild Farm/project/animals/birds.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/04. Wild Farm/project/animals/birds.py
@@ -1,9 +1,12 @@
-from .animal import Bird
+from project.animals.animal import Bird
+from project.food import Vegetable, Fruit, Meat, Seed
 
 
 class Owl(Bird):
 	def __init__(self, name, weight, wing_size):
 		super().__init__(name, weight, wing_size)
+		self.acceptable_foods = [Meat]
+		self.weight_per_food = 0.25
 
 	def make_sound(self):
 		return "Hoot Hoot"
@@ -12,6 +15,8 @@ class Owl(Bird):
 class Hen(Bird):
 	def __init__(self, name, weight, wing_size):
 		super().__init__(name, weight, wing_size)
+		self.acceptable_foods = [Vegetable, Fruit, Meat, Seed]
+		self.weight_per_food = 0.35
 
 	def make_sound(self):
 		return "Cluck"
--- Python OOP/Polymorphism and Abstraction - Exercise/04. Wild Farm/project/animals/mammals.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/04. Wild Farm/project/animals/mammals.py
@@ -1,9 +1,12 @@
-from .animal import Mammal
+from project.animals.animal import Mammal
+from project.food import Vegetable, Fruit, Meat, Seed
 
 
 class Mouse(Mammal):
 	def __init__(self, name, weight, living_region):
 		super().__init__(name, weight, living_region)
+		self.acceptable_foods = [Vegetable, Fruit]
+		self.weight_per_food = 0.1
 
 	def make_sound(self):
 		return "Squeak"
@@ -12,14 +15,18 @@ class Mouse(Mammal):
 class Dog(Mammal):
 	def __init__(self, name, weight, living_region):
 		super().__init__(name, weight, living_region)
+		self.acceptable_foods = [Meat]
+		self.weight_per_food = 0.4
 
 	def make_sound(self):
-		return "Woof"
+		return "Woof!"
 
 
 class Cat(Mammal):
 	def __init__(self, name, weight, living_region):
 		super().__init__(name, weight, living_region)
+		self.acceptable_foods = [Vegetable, Meat]
+		self.weight_per_food = 0.3
 
 	def make_sound(self):
 		return "Meow"
@@ -28,6 +35,8 @@ class Cat(Mammal):
 class Tiger(Mammal):
 	def __init__(self, name, weight, living_region):
 		super().__init__(name, weight, living_region)
+		self.acceptable_foods = [Meat]
+		self.weight_per_food = 1
 
 	def make_sound(self):
 		return "ROAR!!!"
