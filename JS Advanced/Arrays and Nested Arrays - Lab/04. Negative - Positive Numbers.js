1f68e40489f5d04611afb4e8e98d442a12052465
hristo-grudev <hr.grudev@gmail.com>
2021-08-23 15:07:35 +0300

    js

---
+++ JS Advanced/Arrays and Nested Arrays - Lab/04. Negative - Positive Numbers.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    let result = [];
+    for (const num of input){
+        if (num <0) {
+            result.unshift(num);
+        } else {
+            result.push(num);
+        }
+    }
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+    
+    }
+}
