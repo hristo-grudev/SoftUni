005efc10523943222fd5ce44287ef5be6bd4b1d3
hristo-grudev <hr.grudev@gmail.com>
2021-10-04 15:29:33 +0300

    2

--- JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content');
+   content.addEventListener('click', reveal);
+
+   for (let word of words){
+      const div = document.createElement('div');
+      const para = document.createElement('p');
+
+      para.textContent = word;
+      para.style.display = 'none';
+      div.appendChild(para);
+
+      div.addEventListener('click', reveal);
+      content.appendChild(div);      
+   }
+   function reveal(e) {
+      if (e.target.tagNme == 'DIV' && e.target != content){
+      e.currentTarget.children[0].style.display = '';
+      }
+   }
 }
--- JS Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,37 @@
 function attachEventsListeners() {
+    const rations = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    }
+    
+    function convert(value, unit) {
+        const idDays = value / rations[unit];
 
-    console.log('TODO:...');
+        return {
+            days: idDays,
+            hours: idDays * rations.hours,
+            minutes: idDays * rations.minutes,
+            seconds: idDays * rations.seconds
+        }
+    }
+    const daysInput = document.getElementById('days');
+    const hoursInput = document.getElementById('hours');
+    const minutesInput = document.getElementById('minutes');
+    const secondsInput = document.getElementById('seconds');
+
+    document.querySelector('main').addEventListener('click', onConvert);
+
+    function onConvert(e) {
+        if (e.target.tagName = 'INPUT' && e.target.type == 'button') {
+            const input = e.target.parentElement.querySelector('input[type="text"]');
+
+            const time = convert(Number(input.value), input.id);
+            daysInput.value = time.days;
+            hoursInput.value = time.hours;
+            minutesInput.value = time.minutes;
+            secondsInput.value = time.seconds;
+        }
+    }
 }
