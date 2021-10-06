5435da6385e845ab4b0c7d2dff70e57bdb401083
hristo-grudev <hr.grudev@gmail.com>
2021-10-06 16:46:08 +0300

    6

--- JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(e) {
+        const profile = e.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            const infoDIf = profile.querySelector('div');
+            // const infoDif = Array
+            // .from(e.target.parentElement.querySelectorAll('div'))
+            // .find(d => d.id.includes('HiddenFields'));
+
+            if (e.target.textContent == 'Show more') {
+                infoDIf.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                infoDIf.style.display = '';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
