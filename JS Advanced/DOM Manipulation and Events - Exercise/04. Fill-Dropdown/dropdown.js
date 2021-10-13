a78ecf9e61f0276c28b834cd5e00b780c8fb0c7c
hristo-grudev <hr.grudev@gmail.com>
2021-10-13 16:50:12 +0300

    6

--- JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let item_text = document.getElementById('newItemText');
+    let item_value = document.getElementById('newItemValue');
+    let menu = document.getElementById('menu');
+
+    let option = document.createElement('option');
+    option.innerHTML = item_text.value;
+    option.value = item_value.value;
+
+    menu.appendChild(option);
+
+    item_text.value = '';
+    item_value.value = '';
+
 }
