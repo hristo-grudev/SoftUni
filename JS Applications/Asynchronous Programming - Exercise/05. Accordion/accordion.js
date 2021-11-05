36388174b94b32d5d4f551384cfcc388e9b068d6
hristo-grudev <hr.grudev@gmail.com>
2021-11-05 11:51:29 +0200

    home work

--- JS Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
+++ JS Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
@@ -1,18 +1,47 @@
-function solution() {
-    let button = document.querySelector(`span.button`);
-    let butonName = document.getElementsByClassName(`button`)[0];
-    let divHiden = document.getElementById(`extra`);
- 
-    button.addEventListener(`click`,()=>{
- 
-        if(butonName.textContent == `More`) {
-            divHiden.style.display = `block`;
-            butonName.textContent = `Less`;
-        } else if(butonName.textContent == `Less`) {
-            
-                divHiden.style.display = `none`;
-                butonName.textContent = `More`;
-        }
-       
-    })
+function eFactory (tag, className = '', content = '') {
+	const e = document.createElement(tag)
+	e.className = className
+	e.textContent = content
+
+	return e
 }
+
+function template ({ _id, title }) {
+	const wrapper = eFactory('div', 'accordion')
+	const headDiv = eFactory('div', 'head')
+	const titleSpan = eFactory('span', '', title)
+	const btn = eFactory('button', 'button', 'More')
+	const extraDiv = eFactory('div', 'extra')
+	extraDiv.style.display = 'none'
+	const contentParagraph = eFactory('p')
+	btn.id = _id
+
+	headDiv.append(titleSpan, btn)
+	extraDiv.appendChild(contentParagraph)
+	wrapper.append(headDiv, extraDiv)
+
+	btn.addEventListener('click', async () => {
+		if (extraDiv.style.display === 'none') {
+			const data = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${_id}`)
+			const desData = await data.json()
+			btn.textContent = 'Less'
+			extraDiv.style.display = 'block'
+			contentParagraph.textContent = desData.content
+		} else {
+			btn.textContent = 'More'
+			extraDiv.style.display = 'none'
+		}
+	})
+
+	return wrapper
+}
+
+async function solution () {
+	const output = document.getElementById('main')
+	const titles = await fetch('http://localhost:3030/jsonstore/advanced/articles/list')
+	const desTitles = await titles.json()
+
+	desTitles.forEach(x => output.appendChild(template(x)))
+}
+
+document.addEventListener('DOMContentLoaded', solution)
