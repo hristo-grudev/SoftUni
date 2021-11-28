c3c0f4e7b60f11cf8257b01f7fa84e409e761139
hristo-grudev <hr.grudev@gmail.com>
2021-11-28 21:28:34 +0200

    exam prep 1

---
+++ JS Applications/Exam Preparation 1/Meme_Lounge/src/app.js
@@ -0,0 +1,20 @@
+
+import { page, remder } from './lib.js';
+import * as api from './api/data.js'
+import { homePage } from './views/home.js';
+import { catalogPage } from './views/catalog.js';
+
+const root = document.querySelector('main');
+
+page(decorateContext);
+page('/', homePage);
+page('/memes', catalogPage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+	ctx.render = (content) => render(content, root);
+	ctx.updateUserNav = updateUserNav;
+
+	next();
+}
